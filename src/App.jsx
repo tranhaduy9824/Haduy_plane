159d9f685f5b5c9cfa4dc73f82e2f15a8b70f235
tranhaduy204 <tranhaduy204@gmail.com>
2024-07-13 16:19:21 +0700

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { ScrollControls } from "@react-three/drei";
+import { EffectComposer } from "@react-three/postprocessing";
+import { Noise } from "lamina";
+import { Overlay } from "./components/Overlay";
+import { usePlay } from "./contexts/Play";
 
 function App() {
+  const { play, end } = usePlay();
+
   return (
     <>
-      <Canvas camera={{
-        position: [0, 0, 5],
-        fov: 30,
-      }}>
+      <Canvas>
         <color attach="background" args={["#ececec"]} />
-        <Experience />
+        <ScrollControls
+          pages={play && !end ? 20 : 0}
+          damping={0.5}
+          styles={{
+            top: "10px",
+            left: "0px",
+            bottom: "10px",
+            right: "10px",
+            width: "auto",
+            height: "auto",
+            transition: "opacity 1.4s ease-in-out",
+            opacity: play ? 1 : 0,
+          }}
+        >
+          <Experience />
+        </ScrollControls>
       </Canvas>
+      <Overlay />
     </>
   );
 }
---
+++ src/components/Overlay.jsx
@@ -0,0 +1,31 @@
+import { useProgress } from "@react-three/drei";
+import { usePlay } from "../contexts/Play";
+
+export const Overlay = () => {
+  const { progress } = useProgress();
+  const { play, setPlay, end, hasScroll } = usePlay();
+
+  return (
+    <div
+      className={`overlay ${play ? "overlay--disable" : ""} ${
+        hasScroll ? "overlay--scrolled" : ""
+      }`}
+    >
+      <div
+        className={`loader ${progress === 100 ? "loader--disappear" : ""}`}
+      />
+      {progress === 100 && (
+        <div className={`intro ${play ? "intro--disappear" : ""}`}>
+          <h1 className="logo">HADYNE</h1>
+          <p className="intro__scroll">Scroll to begin the journ</p>
+          <button className="explore" onClick={() => setPlay(true)}>
+            Explore
+          </button>
+        </div>
+      )}
+      <div className={`outro ${end ? "outro--appear" : ""}`}>
+        <p className="outro__text">Wish you had a great flight with us...</p>
+      </div>
+    </div>
+  );
+};
